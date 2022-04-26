d2208ed394c5fcc3a7aec37c23d73630900f3449
Deepesh Patel <80380848+Deepesh-Patel@users.noreply.github.com>
2022-04-26 14:14:45 +0530

    Add files via upload

---
+++ Basic Programs/05_area_of_rectangle.py
@@ -0,0 +1,10 @@
+# Write a python program to find area of rectangle entered by user.
+
+l = input("Enter length of rectangle: " )
+b = input("Enter breadth of rectangle: ")
+l = int(l)
+b = int(b)
+
+area = l*b;
+
+print("Area of rectangle: ", area)
