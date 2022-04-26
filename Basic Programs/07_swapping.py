df965c86ec32e89e356693b5956eb967cf00d3e5
Deepesh Patel <80380848+Deepesh-Patel@users.noreply.github.com>
2022-04-26 14:45:31 +0530

    Add files via upload

---
+++ Basic Programs/07_swapping.py
@@ -0,0 +1,18 @@
+s1 = input("Enter 1st number: ")
+s2 = input("Enter 2nd number: ")
+
+s1 = int(s1)
+s2 = int(s2)
+
+print("****** BEFORE SWAPPING ******")
+print("s1: ", s1)
+print("s2: ", s2)
+
+temp = s1
+s1 = s2
+s2 = temp
+
+print("****** AFTER SWAPPING ******")
+print("s1: ", s1)
+print("s2: ", s2)
+
