8e4203665ee11a114edde0879e81f8f6c2cf5a97
Deepesh Patel <80380848+Deepesh-Patel@users.noreply.github.com>
2022-04-26 11:10:20 +0530

    Add files via upload

---
+++ Basic Programs/03_average.py
@@ -0,0 +1,10 @@
+# Write a python program to find average of 2 numbers entered by user.
+
+a = input("Enter 1st number: ")
+b = input("Enter 2nd number: ")
+a = int(a)
+b = int(b)
+
+avg = (a+b)/2 
+
+print("Average of a & b: ", avg)
