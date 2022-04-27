33fb444b9713976ca9d03041d13e081e73b91eba
Deepesh Patel <80380848+Deepesh-Patel@users.noreply.github.com>
2022-04-27 14:50:18 +0530

    Add files via upload

---
+++ Strings/01_string_functions.py
@@ -0,0 +1,9 @@
+story = "once upon a time there was a boy named buddy who learned python course with notes"
+
+#string functions
+print(len(story))
+print(story.endswith("notes"))
+print(story.count("o"))
+print(story.capitalize())
+print(story.find("there"))
+print(story.replace("buddy", "code_buddy"))
