67d3efcdad9ef3fdc59d52806440cdb5d282e8e0
Deepesh Patel <80380848+Deepesh-Patel@users.noreply.github.com>
2022-04-27 09:29:39 +0530

    Add files via upload

---
+++ Basic Programs/09_add_student_info.py
@@ -0,0 +1,11 @@
+print("****** ENTER STUDENT DETAILS ******")
+
+id = int(input("Enter the roll no of student: "))
+name = str(input("Enter the name of student: "))
+course = str(input("Enter the course of student: "))
+city = str(input("Enter city of student: "))
+
+print("\n")
+print("****** STUDENT DETAILS ******")
+
+print("roll no: ", id,"\nname: ", name,"\ncourse: ", course,"\ncity: ", city)
