efffc6d59a83ce78dcb817c52a147302339ef90f
Deepesh Patel <80380848+Deepesh-Patel@users.noreply.github.com>
2022-04-28 10:32:41 +0530

    Add files via upload

---
+++ Lists & Tuples/01_lists_practice.py
@@ -0,0 +1,16 @@
+# Create a list using []
+a = [1, 2 , 4, 68, 6, 7, 88]
+
+# Print the list using print() function
+print(a)
+
+# Access using index using a[0], a[1], a[2]
+print(a[4])
+
+# Change the value of list using
+a[0] = 70
+print(a)
+
+# We can create a list with items of different types
+c = [45, "Buddy", False, 6.9]
+print(c)
