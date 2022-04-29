1bc2f1f192ad171512224efa70ca9d1a98d3e7bc
Deepesh Patel <80380848+Deepesh-Patel@users.noreply.github.com>
2022-04-29 10:12:04 +0530

    Add files via upload

---
+++ Lists & Tuples/03_tuple_methods.py
@@ -0,0 +1,5 @@
+#Creating a tuple using ()
+t1 = (1,2,4,6,2,4,5,8,7,2,5,2)
+
+print("Tuple_count: ", t1.count(2))      #t1.count(2) return no. of times 2 occur in t1
+print("Tuple_index: ", t1.index(6))       #t1.index(6) return the index of 6th occurance of 3 in t1
