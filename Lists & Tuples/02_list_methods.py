664d6a0f1307de9b217b0df515c49332b2ece26a
Deepesh Patel <80380848+Deepesh-Patel@users.noreply.github.com>
2022-04-29 09:50:54 +0530

    Add files via upload

---
+++ Lists & Tuples/02_list_methods.py
@@ -0,0 +1,19 @@
+li1 = [1, 4, 7, 24, 15, 10, 29, 46, 35, 3]
+
+li1.sort()       #sorts the list
+print("List_sort: ",li1)
+
+li1.reverse()  #reverses the list
+print("List_reverse: ",li1)
+
+li1.append(64)   #add 64 at the end of the list
+print("List_append: ", li1)
+
+li1.insert(4, 645)    #inserts 645 at index 4
+print("List_insert: ", li1)
+
+li1.pop(5)     #removes element at index 5
+print("List_pop: ", li1)
+
+li1.remove(29)     #removes 29 from the list
+print("List_remove: ", li1)
