8e16305b10920565312c18200fe5c6fe5b660ffc
Deepesh Patel <80380848+Deepesh-Patel@users.noreply.github.com>
2022-04-29 11:50:56 +0530

    Add files via upload

---
+++ Lists & Tuples/06_tuple_count.py
@@ -0,0 +1,5 @@
+''' Write a python program to count number of zeroes in the following tuple.
+      t = (7, 0, 8, 0, 0, 9, 0, 6)  '''
+      
+t = (7, 0, 8, 0, 0, 9, 6)
+print("Tuple_count: ", t.count(0)) 
