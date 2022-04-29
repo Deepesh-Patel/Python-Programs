cd21fb40c75d1482783de833a191d9798feec4ba
Deepesh Patel <80380848+Deepesh-Patel@users.noreply.github.com>
2022-04-29 11:46:52 +0530

    Add files via upload

---
+++ Lists & Tuples/04_store_fruits.py
@@ -0,0 +1,13 @@
+#Write a python program to store 7 fruits in a list entered by the user.
+
+f1 = input("Enter name of fruit no 1: ")
+f2 = input("Enter name of fruit no 2: ")
+f3 = input("Enter name of fruit no 3: ")
+f4 = input("Enter name of fruit no 4: ")
+f5 = input("Enter name of fruit no 5: ")
+f6 = input("Enter name of fruit no 6: ")
+f7 = input("Enter name of fruit no 7: ")
+
+fruitlist = [f1, f2, f3, f4, f5, f6, f7]
+print("\n****** FRUITS ENTERED ******")
+print(fruitlist)
