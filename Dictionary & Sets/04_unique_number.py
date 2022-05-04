dd4cc738fe5257a8bcc9bf3547e06ef62b6581f4
Deepesh Patel <80380848+Deepesh-Patel@users.noreply.github.com>
2022-05-04 10:44:35 +0530

    Add files via upload

---
+++ Dictionary & Sets/04_unique_number.py
@@ -0,0 +1,14 @@
+# Write a python program to input 8 numbers from the user and display all the unique numbers (once)
+
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+num3 = int(input("Enter 3rd number: "))
+num4 = int(input("Enter 4th number: "))
+num5 = int(input("Enter 5th number: "))
+num6 = int(input("Enter 6th number: "))
+num7 = int(input("Enter 7th number: "))
+num8 = int(input("Enter 8th number: "))
+
+s = {num1, num2, num3, num4, num5, num6, num7, num8}
+print("\n")
+print(s)
