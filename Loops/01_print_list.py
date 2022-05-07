3a3a7b073795df7a81ef9cbe3c08c08142df94a3
Deepesh Patel <80380848+Deepesh-Patel@users.noreply.github.com>
2022-05-07 09:52:07 +0530

    Add files via upload

---
+++ Loops/01_print_list.py
@@ -0,0 +1,8 @@
+# Write a python program to print the content of a list using while loops.
+
+fruits = ['Banana', 'Watermelon', 'Mangoes', 'Grapes',  'Kiwi', 'Oranges']
+
+i = 0
+while i < len(fruits):
+	print(fruits[i])
+	i = i + 1
