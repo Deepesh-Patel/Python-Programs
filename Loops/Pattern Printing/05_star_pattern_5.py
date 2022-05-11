ea1153d866c70a076b0697ff352531a50fa760d5
Deepesh Patel <80380848+Deepesh-Patel@users.noreply.github.com>
2022-05-11 09:44:10 +0530

    Add files via upload

---
+++ Loops/Pattern Printing/05_star_pattern_5.py
@@ -0,0 +1,22 @@
+''' Write a program to print the following star pattern
+		*
+	        * *
+		* * *
+		* * * *
+		* * * * *
+		* * * *
+		* * *
+		* *
+		*           '''
+					
+rows = int(input("Enter the number of rows: "))
+  
+for i in range(0, rows):  
+    for j in range(1, i + 1):  
+        print("*", end=' ')  
+    print(" ")  
+
+for i in range(rows + 1, 0, -1):  
+    for j in range(0, i - 1):  
+        print("*", end=' ')  
+    print(" ")  
