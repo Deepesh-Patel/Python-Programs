fd079a1567609ec69cfa1f70f3c048be9aeaa059
Deepesh Patel <80380848+Deepesh-Patel@users.noreply.github.com>
2022-08-21 10:36:54 +0530

    Add files via upload

---
+++ File I\O/02_readline.py
@@ -0,0 +1,12 @@
+f = open('E:\Deepesh\Python\File I O\sample02.txt')
+
+data = f.readline()    #read 1st line
+print(data)
+
+data = f.readline()    #read 2nd line
+print(data)
+
+data = f.readline()    #read 3rd line
+print(data)
+
+f.close()
